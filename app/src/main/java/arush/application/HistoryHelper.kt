c1562d1fe47a3267a3b867edeee92ee002a00a28
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-06-28 22:31:18 +0530

    file history class creation

---
+++ app/src/main/java/arush/application/HistoryHelper.kt
@@ -0,0 +1,12 @@
+package arush.application
+
+class HistoryHelper {
+    fun accountFileCreator(userId: String)
+    {}
+    fun fileOpener(fileName: String)
+    {}
+    fun setHistory()
+    {}
+    fun getHistory(userId: String)
+    {}
+}
