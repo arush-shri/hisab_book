4d35164ae0b71b2c73e298d3719cff576644f67e
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-07-12 20:34:50 +0530

    history card floating button add

--- app/src/main/java/arush/application/HistoryActivity.kt
+++ app/src/main/java/arush/application/HistoryActivity.kt
@@ -2,15 +2,24 @@ package arush.application
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import arush.application.databinding.ActivityHistoryBinding
 
 class HistoryActivity : AppCompatActivity() {
 
     private val userId= intent.getStringExtra("user_id").toString()
     private val historyHelper : HistoryHelper = HistoryHelper(this@HistoryActivity)
+    private lateinit var historyBinding: ActivityHistoryBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_history)
+        historyBinding = ActivityHistoryBinding.inflate(layoutInflater)
+        val view = historyBinding.root
+        setContentView(view)
+
+        historyBinding.deleteHistoryButton.setOnClickListener{
+
+        }
+
 
     }
     private fun getHistory()
